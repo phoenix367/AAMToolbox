ab691f387c886f0dacd6abf82bc17e7827f95b1f
Ivan Gubochkin <jhng@yandex.ru>
2015-11-02 21:06:25 +0300

    Simple AAM object tracking

--- utils/aam_capture/aam_capture.cpp
+++ utils/aam_capture/aam_capture.cpp
@@ -17,14 +17,68 @@
  */
 
 #include "tclap/CmdLine.h"
-#include "aam/aam.h"
+#include "aam/AAMEstimator.h"
+
+#include <opencv2/opencv.hpp>
 
 int main(int argc, char* argv[]) {
     try {
         TCLAP::CmdLine cmd("AAM live capture utility", ' ', "0.1");
 
+        TCLAP::ValueArg<std::string> modelArg("m", "aam-model", "Active appearance model file",
+                true, "", "string", cmd);
+        TCLAP::ValueArg<std::string> cascadesArg("c", "haar-cascades", "Path to file with Haar cascades",
+                true, "", "string", cmd);
+
         // Parse the argv array.
         cmd.parse( argc, argv );
+        
+        aam::AAMEstimator estimator;
+        
+        estimator.load(modelArg.getValue());
+
+        cv::VideoCapture cap(0); // open the default camera
+        if (!cap.isOpened()) {  // check if we succeeded
+            std::cerr << "Can't open camera capture" << std::endl;
+            return -1;
+        }
+        
+        cv::CascadeClassifier classifier;
+        if (!classifier.load(cascadesArg.getValue())) {
+            std::cerr << "Can't load Haar cascades" << std::endl;
+            return -1;
+        }
+
+        for(;;)
+        {
+            cv::Mat frame;
+            cap >> frame; // get a new frame from camera
+            
+            cv::Mat grayFrame;
+            cv::cvtColor(frame, grayFrame, CV_BGR2GRAY);
+            
+            std::vector<cv::Rect> objects;
+            classifier.detectMultiScale(grayFrame, objects, 1.3, 5);
+            
+            for (size_t i = 0; i < objects.size(); i++) {
+                aam::Vertices2DList foundPoints;
+                estimator.estimateAAM(frame, aam::Point2D(objects[i].x + objects[i].width / 2,
+                        objects[i].y + objects[i].height / 2), foundPoints);
+                
+                for (int j = 0; j < foundPoints.size(); j++)
+                {
+                    cv::circle(frame, foundPoints[j], 3, CV_RGB(255, 255, 255));
+                }
+                
+                cv::rectangle(frame, objects[i], cv::Scalar(0, 255, 0), 3);
+                
+            }
+            
+            cv::imshow("edges", frame);
+            if (cv::waitKey(30) >= 0) {
+                break;
+            }
+        }
     } catch (TCLAP::ArgException &e) {
         std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
     } catch (std::exception& e) {
